6e6d045084a95696c54c55cb122281759b84839f
Kimyechan2154 <kimyechan2959@gmail.com>
2024-10-06 17:19:00 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,86 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main(){
+    int a;
+    
+    scanf("%d" ,&a);
+    
+    if(a >= 90){
+        printf("A");
+    }
+   else if(a >= 80){
+        printf("B");
+    }
+    else if(a >= 70){
+        printf("C");
+    }
+   else if(a >= 60){
+        printf("D");
+    }
+   else{
+       printf("F");
+   }
+   
+    return 0;
+}
+
+
+
+// int main(){
+    
+    
+    
+//   int a = 5, b = 5, c;
+   
+//   a--;
+//   b--;
+   
+//   printf("%d %d\n", a, b);
+   
+//   a = 5;
+//   c = --a;
+//   printf ("%d %d", a, c);
+    
+    
+    
+//     return 0;
+// }
+
+
+// int main(){
+//     int num1 = 10;
+//     float num2;
+//     char num3;
+    
+//     num2 = 1.025, num3 = 'c';
+    
+//     printf("%d %.4f %c\n", num1, num2, num3);
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
