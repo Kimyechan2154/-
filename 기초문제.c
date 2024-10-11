4f7319cc9944ccbd5f2d5cb0ee6d56251a56b19f
Kimyechan2154 <kimyechan2959@gmail.com>
2024-10-11 23:00:32 +0900

    Add files via upload

---
+++ 기초문제.c
@@ -0,0 +1,72 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+
+
+
+// int main(){
+    
+//     int a, b, c, i;
+    
+//     scanf("%d %d %d", &a, &b, &c);
+    
+//     if(a == b){
+//         if (a==c){
+//             printf("%d", a*1000+10000);
+//         }
+//         else{
+//             printf("%d", a*100+1000);
+//         }
+//     }
+    
+//     else{
+//         if (a == c){
+//             printf("%d", a*100+1000);
+//         }
+//         else if(b == c){
+//             printf("%d", b*100+1000);
+//         }
+//         else{
+//             if(a<b)
+//             a = b;
+//             {
+//                 if(a<c)
+//                 a = c;
+//             }
+//         printf("%d", a*100);
+//         }
+        
+//     }
+    
+//     return 0;
+// }
+
+// int main(){
+    
+//     long long a, b, c;
+    
+//     scanf("%ld %ld %ld", &a, &b, &c);
+    
+//     printf("%ld", a+b+c);
+    
+//     return 0;
+// }
+
+
+// int main(){
+//   char arr[6] = {"hello"};
+   
+//   char* ptr;
+   
+//   ptr = &arr[0];
+   
+//   printf("%c", *(ptr+4));
+   
+//   return 0;
+   
+// }
