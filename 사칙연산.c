3907f72f5e39b8b4f2613906a54eed4d9919e90f
Kimyechan2154 <kimyechan2959@gmail.com>
2024-10-12 20:27:24 +0900

    Add files via upload

---
+++ 사칙연산.c
@@ -0,0 +1,68 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+ 
+ printf("\    /\")
+ 
+    return 0;
+}
+
+// #include <stdio.h>
+
+// int main()
+// {
+    
+//  int a, b, c;
+ 
+//  scanf("%d", &a);
+ 
+//  scanf("%d", &b);
+ 
+ 
+ 
+//  printf("%d\n", a*(b%10));
+//  printf("%d\n", (a*(b%100-b%10))/10);
+//  printf("%d\n", (a*(b%1000-b%100))/100);
+//  printf("%d", a*b);
+ 
+ 
+ 
+//     return 0;
+// }
+
+
+// int main()
+// {
+    
+//  char a[50];
+ 
+//  scanf("%s", &a);
+ 
+//  printf("%s??!", a);
+ 
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+    
+//   double a, b;
+//   scanf("%lf %lf", &a, &b);
+   
+   
+//   printf("%0.9lf", a/b);
+   
+//     return 0;
+// }
